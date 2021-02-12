254a66252c238e1cc8247e2cd2aaf96e8ffb55d6
hnsorens <57865966+hnsorens@users.noreply.github.com>
2021-02-11 19:58:41 -0500

    Update stembot2.py

--- stembot2.py
+++ stembot2.py
@@ -16,6 +16,7 @@ name = "cap"
 quoteResponseWaiting = False
 quoteResponseTimer = 0
 quote = ""
+quotter
 
 @client.event
 async def on_ready():
@@ -30,6 +31,7 @@ async def on_message(message):
   global quoteResponseWaiting
   global quoteResponseTimer
   global quote
+  global quotter
   quoteChannel = client.get_channel(809524819661684758)
   if (message.author == client.user):
       return
@@ -41,15 +43,16 @@ async def on_message(message):
     await message.channel.send(command[13:] + " is a " + str(random.randint(0,10)) + "/10 on the rainbow flag scale")
 
   if (message.content.startswith("!quote")):
+    quotter = message.author
     quoteResponseTimer = 0
     await message.channel.send("Are you sure you want to quote " + "\"" + lastMessage.content + "\" by " + '{}'.format(lastMessage.author.mention) + " y/n")
     quote = lastMessage
     quoteResponseWaiting = True
   if (quoteResponseWaiting):
     quoteResponseTimer += 1
-    if (message.content == "y"):
-      await quoteChannel.send("\"" + quote.content + "\"\n-" + '{}'.format(quote.author.mention))
-    if (quoteResponseTimer > 5 or message.content == "n"):
+    if (message.content == "y" and message.author == quotter):
+      await quoteChannel.send("\"" + quote.content + "\"\n-" + '{}'.format(quote.author.mention) + "\n")
+    if (quoteResponseTimer > 5 or message.content == "n" and message.author == quotter):
       quoteResponseWaiting = False
       quoteResponseTimer = 0
 
